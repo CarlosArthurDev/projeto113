9ce78593c032c69e81ad18e43b82bab11bd556f6
CarlosArthurDev <103152440+CarlosArthurDev@users.noreply.github.com>
2022-12-02 22:11:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload(){
+
+
+
+}
+
+function setup(){
+    canvas=createCanvas(500,500)
+    canvas.position(500,300)
+    video=createCapture(VIDEO)
+    video.hide()
+    tinticolor=""
+}
+function draw(){
+    image(video,0,0,450,450)
+    circle(450,450)
+    
+}
+function filtro(){
+
+    tinticolor=document.getElementById("inputColor").value
+}
